3f3f4ea2941c86e3c090ef3bb8002d8e0762504f
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2021-10-21 16:25:42 +0700

    dfgdfg

--- MultipeChoiceTest/views.py
+++ MultipeChoiceTest/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+
+
+def index(request):
+    subjects = Subject.objects.all()
+
+    context = {
+        'subjects': subjects
+    }
+    return render(request, 'index.html', context)
