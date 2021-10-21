70a70d7447b8253044f965058665b0d90446bef1
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2021-10-21 10:13:30 +0700

    j

--- MultipeChoiceTest/admin.py
+++ MultipeChoiceTest/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+@admin.register(Student,
+Teacher,
+Subject,
+Question,
+Choice,
+Exam,
+StudentExam,
+StudentExamQuestion
+)
+
+class StudentAdmin(admin.ModelAdmin):
+    pass
+
+class TeacherAdmin(admin.ModelAdmin):
+    pass
+
+class SubjectAdmin(admin.ModelAdmin):
+    pass
+
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+
+class ChoiceAdmin(admin.ModelAdmin):
+    pass
+
+class ExamAdmin(admin.ModelAdmin):
+    pass
+
+class StudentExamAdmin(admin.ModelAdmin):
+    pass
+
+class StudentExamQuestionAdmin(admin.ModelAdmin):
+    pass
--- MultipeChoiceTest/models.py
+++ MultipeChoiceTest/models.py
@@ -1,9 +1,71 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.base import Model
 
 # Create your models here.
 
 class Student(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+class Teacher(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+class ExamAbstract(models.Model):
+    title = models.CharField(max_length=200)
+
+    class Meta:
+        abstract = True
+
+class Subject(ExamAbstract):
+    
+    def __str__(self):
+        return self.title
+
+class Question(ExamAbstract):
+    exam_c = models.ForeignKey("Exam", on_delete=models.CASCADE)
+    choices = models.ManyToManyField("Choice", blank=True)
+
+    def __str__(self):
+        return ('%s', '%s', self.exam_c, self.choices)
+
+    
+
+class Choice(ExamAbstract):
+    question_c = models.ForeignKey(Question, on_delete=models.CASCADE)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return (self.question_c)
+
+
+class Exam(ExamAbstract):
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    questions = models.ManyToManyField(Question, blank=True)
+    date_created = models.DateField(auto_now=True)
+
+
+
+class StudentExam(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (self.student, self.exam)
+
+class StudentExamQuestion(models.Model):
+    student_exam = models.ForeignKey(StudentExam, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    student_choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (self.student_exam, self.question, self.student_choice)
+
     
 
